4b6a7b75a43ac3583dcf083e893d16282fc08365
Tony Fahy <fahy_tony@hotmail.com>
2021-06-24 02:00:33 +0100

    Working AngularV9

--- src/data-models/pokemon.ts
+++ src/data-models/pokemon.ts
@@ -4,11 +4,14 @@ export class Pokemon {
   id?: string;
   details?: PokemonDetails;
   description?: string;
+  favourite: boolean;
 }
 
 export class PokemonDetails {
   name: string;
   id: number;
+  height: number;
+  weight: number;
   sprites: Sprites;
   abilities?: Array<any>;
   types?: Array<any>;
