94467808f844a715ce0743c062dd485d6b61437b
Tony Fahy <fahy_tony@hotmail.com>
2021-06-26 05:30:16 +0100

    prod env set back to false

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   pokemonApiUrl: 'https://pokeapi.co/api/v2/',
   pageSize: 20,
 };
